e640394c4cbff90693ed86c324264f55eb7bc1f6
Kan Aleksandr <alx.kan037@gmail.com>
2023-01-26 16:07:43 +0500

    Add WorkoutCell and WorkoutViewForCell files

---
+++ TestFirst_App/Views/MainVC/Cells/WorkoutTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  WorkoutTableViewCell.swift
+//  TestFirst_App
+//
+//  Created by Aleksandr Kan on 26/01/23.
+//
+
+import UIKit
+
+class WorkoutTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        
+    }
+    
+    private func setConstraints(){
+        
+    }
+    
+}
---
+++ TestFirst_App/Views/MainVC/View for Cell/WorkoutViewForCell.swift
@@ -0,0 +1,21 @@
+//
+//  WorkoutViewForCell.swift
+//  TestFirst_App
+//
+//  Created by Aleksandr Kan on 26/01/23.
+//
+
+import UIKit
+
+class WorkoutViewForCell: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+}
