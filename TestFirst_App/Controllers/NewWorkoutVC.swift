11bf980eda37d8b0f6a49832a63b279ad4f1ab59
Kan Aleksandr <alx.kan037@gmail.com>
2023-01-30 11:15:54 +0500

    Add closeButton in NewWorkoutVC

--- TestFirst_App/Controllers/NewWorkoutVC.swift
+++ TestFirst_App/Controllers/NewWorkoutVC.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class NewWorkoutVC: UIViewController {
+    
+    private let closeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "closeButton"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,12 +23,26 @@ class NewWorkoutVC: UIViewController {
         setupViews()
         setupConstraints()
     }
+    
+    override func viewDidLayoutSubviews() {
+        closeButton.layer.cornerRadius = closeButton.frame.width / 2
+    }
 
     private func setupViews() {
         view.backgroundColor = .specialBackground
+        view.addSubview(closeButton)
     }
     
     private func setupConstraints() {
+        
+        NSLayoutConstraint.activate([
+            closeButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            closeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -5),
+            closeButton.widthAnchor.constraint(equalToConstant: 30),
+            closeButton.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        
         
     }
     
