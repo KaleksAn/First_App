7042a56166b022885d3c8edc332ae5b65a04795c
Kan Aleksandr <alx.kan037@gmail.com>
2023-01-30 12:23:15 +0500

    Add textField and labels on NewWorkoutVC

--- TestFirst_App/Controllers/NewWorkoutVC.swift
+++ TestFirst_App/Controllers/NewWorkoutVC.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class NewWorkoutVC: UIViewController {
     
-    private let closeButton: UIButton = {
+    private lazy var closeButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setImage(UIImage(named: "closeButton"), for: .normal)
+        let image = UIImage(named: "closeButton")?.withRenderingMode(.alwaysOriginal)
+        button.setImage(image, for: .normal)
+        button.addTarget(self, action: #selector(closeNewWorkoutVC), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
+    }()
+    
+    private let labelWorkoutVC: UILabel = {
+       let label = UILabel()
+        label.text = "NEW WORKOUT"
+        label.font = .robotMedium24()
+        label.textColor = .specialBlack
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let labelForTextField = UILabel(text: "Name")
+    private let dateAndRepeatLabel = UILabel(text: "Date and Repeat")
+    
+    private let nameTextField: UITextField = {
+       let field = UITextField()
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: field.frame.size.height))
+        field.leftViewMode = .always
+        field.layer.cornerRadius = 10
+        field.textColor = .specialGray
+        field.font = .robotBold20()
+        field.clearButtonMode = .always
+        field.returnKeyType = .done
+        field.backgroundColor = .specialBrown
+        field.placeholder = "Enter your name"
+        field.translatesAutoresizingMaskIntoConstraints = false
+        return field
     }()
     
 
@@ -31,7 +61,25 @@ class NewWorkoutVC: UIViewController {
     private func setupViews() {
         view.backgroundColor = .specialBackground
         view.addSubview(closeButton)
+        view.addSubview(labelWorkoutVC)
+        view.addSubview(labelForTextField)
+        view.addSubview(nameTextField)
+        view.addSubview(dateAndRepeatLabel)
     }
+    
+    //MARK: - Selectors
+    @objc
+    private func closeNewWorkoutVC() {
+        dismiss(animated: true)
+    }
+    
+    
+}
+
+
+//MARK: - Set constraints
+
+extension NewWorkoutVC {
     
     private func setupConstraints() {
         
@@ -42,9 +90,28 @@ class NewWorkoutVC: UIViewController {
             closeButton.heightAnchor.constraint(equalToConstant: 30)
         ])
         
+        NSLayoutConstraint.activate([
+            labelWorkoutVC.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            labelWorkoutVC.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
         
+        NSLayoutConstraint.activate([
+            labelForTextField.topAnchor.constraint(equalTo: labelWorkoutVC.bottomAnchor, constant: 5),
+            labelForTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            nameTextField.topAnchor.constraint(equalTo: labelForTextField.bottomAnchor, constant: 5),
+            nameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            nameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            nameTextField.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            dateAndRepeatLabel.topAnchor.constraint(equalTo: nameTextField.bottomAnchor, constant: 20),
+            dateAndRepeatLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20)
+        ])
         
     }
-    
     
 }
