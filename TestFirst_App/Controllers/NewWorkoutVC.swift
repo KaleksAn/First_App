58eece6cf30533e3942cefa96897dc24e9ae5fa6
Kan Aleksandr <alx.kan037@gmail.com>
2023-01-30 10:42:00 +0500

    Add NewWorkoutVC

---
+++ TestFirst_App/Controllers/NewWorkoutVC.swift
@@ -0,0 +1,21 @@
+//
+//  NewWorkoutVC.swift
+//  TestFirst_App
+//
+//  Created by Aleksandr Kan on 30/01/23.
+//
+
+import UIKit
+
+class NewWorkoutVC: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
