cd19cceecbddb07179ef1c8ae9b86661b34ca844
Kan Aleksandr <alx.kan037@gmail.com>
2023-01-30 13:04:01 +0500

    Add DateAndRepeat view

---
+++ TestFirst_App/Views/NewWorkoutVC/DateAndRepeatView.swift
@@ -0,0 +1,21 @@
+//
+//  DateAndRepeatView.swift
+//  TestFirst_App
+//
+//  Created by Aleksandr Kan on 30/01/23.
+//
+
+import UIKit
+
+class DateAndRepeatView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
