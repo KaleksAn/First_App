b2d309438f5183bffde0c045231d9853c502ca81
Alexander <Kan>
2023-02-01 00:39:55 +0500

    и

--- TestFirst_App/Controllers/StatisticViewController.swift
+++ TestFirst_App/Controllers/StatisticViewController.swift
@@ -12,7 +12,7 @@ class StatisticViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.asd
+        // Do any additional setup after loading the view.asdasd
     }
     
 
